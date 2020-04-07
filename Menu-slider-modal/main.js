e2c7521d90e62a625bd5d846f7e8ddfbe399e319
daoquangthai <quangthai999999@gmail.com>
2020-04-07 21:07:33 +0700

    finsh js medium project 7/20

---
+++ Menu-slider-modal/main.js
@@ -0,0 +1,21 @@
+const btnMenu = document.querySelector('.btn-menu');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+// Hanle eventlistener
+btnMenu.addEventListener('click', () => {
+  document.body.classList.toggle('show-nav');
+});
+
+// Show modal
+open.addEventListener('click', () => modal.classList.add('show-modal'));
+
+// Hide modal
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+// Hide modal on outside click
+window.addEventListener(
+  'click',
+  (e) => (e.target == modal ? modal.classList.remove('show-modal') : '') // false đều được
+);
